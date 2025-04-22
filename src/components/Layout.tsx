bf945c45ae12f413082bf14b9c67bf55a2b9e482
phybyte <phybyte@proton.me>
2025-04-22 12:37:45 +0200

    improve layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,47 +1,59 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <header
-        style={{
-          height: 80,
-          background: "#111",
-          color: "white",
-          padding: "0 20px",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
+    <Flex direction="column" minH="100vh">
+      <Flex
+        as="header"
+        h="80px"
+        bg="black"
+        color="white"
+        px={5}
+        align="center"
+        justify="space-between"
       >
-                  <Link to="/" style={{ marginRight: 15, color: "white" }}>
+        <RouterLink to="/" style={{ textDecoration: "none", color: "white" }}>
+          <HStack gap={3}>
+            <Image src="/exonet.svg" alt="ExoNet logo" h="40px" />
+            <Heading size="md" m={0}>
+              DecentSpace
+            </Heading>
+          </HStack>
+        </RouterLink>
 
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <img
-            src="/exonet.svg"
-            alt="ExoNet logo"
-            style={{ height: 40, marginRight: 10 }}
-          />
-          <h1 style={{ margin: 0, fontSize: "1.5rem" }}>DecentSpace</h1>
-          
-        </div>
-        </Link>
-       
-        <Flex gap={4}>
-          <Link to="/dashboard" style={{ marginRight: 15, color: "white" }}>
-            My Assets
-          </Link>
-          <Link to="/cesium" style={{ color: "white" }}>
-            Viewer
-          </Link>
-        </Flex>
-        {/* </nav> */}
-      </header>
-      <main style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        <HStack gap={4}>
+          <RouterLink
+            to="/dashboard"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            <Text
+              fontSize="md"
+              fontWeight="bold"
+              _hover={{ textDecoration: "underline" }}
+            >
+              My Assets
+            </Text>
+          </RouterLink>
+          <RouterLink
+            to="/cesium"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            <Text
+              fontSize="md"
+              fontWeight="bold"
+              _hover={{ textDecoration: "underline" }}
+            >
+              Viewer
+            </Text>
+          </RouterLink>
+        </HStack>
+      </Flex>
+
+      <Box as="main" flex="1" display="flex" flexDirection="column">
         {children}
-      </main>
-    </div>
+      </Box>
+    </Flex>
   );
 };
