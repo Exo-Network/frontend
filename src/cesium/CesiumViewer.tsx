afa5c791e999335654a09de6d7e40e673cdbeb67
phybyte <phybyte@proton.me>
2025-04-22 14:12:25 +0200

    no token mode

--- src/cesium/CesiumViewer.tsx
+++ src/cesium/CesiumViewer.tsx
@@ -1,14 +1,28 @@
+import { Rectangle, SingleTileImageryProvider } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
-import { Viewer } from "resium";
+import { ImageryLayer, Viewer } from "resium";
 import { GroundStations } from "./utils/GroundStationLoader";
-import {SatellitesEntities} from "./utils/SatelliteLoader";
+import { SatellitesEntities } from "./utils/SatelliteLoader";
 
 export const CesiumViewer = () => {
+  const offline = true;
   return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <Viewer style={{ height: '100%', width: '100%' }}>
-         <GroundStations />
-         <SatellitesEntities />
+    <div style={{ height: "100%", width: "100%" }}>
+      <Viewer style={{ height: "100%", width: "100%" }}>
+        {offline && (
+          <ImageryLayer
+            imageryProvider={
+              new SingleTileImageryProvider({
+                url: "/cesium/natural-earth-2.jpg", // Path relative to public/
+                rectangle: Rectangle.fromDegrees(-180, -90, 180, 90),
+                tileWidth: 1008,
+                tileHeight: 504,
+              })
+            }
+          />
+        )}
+        <GroundStations />
+        <SatellitesEntities />
       </Viewer>
     </div>
   );
